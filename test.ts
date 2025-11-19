5d366d360938e484c2012f181721b38077db39a3
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2025-11-19 18:38:22 +0200

    .

---
+++ test.ts
@@ -0,0 +1,18 @@
+const userName: string = 'Student';
+const age: number = 15;
+const isActive: boolean = true;
+
+const numbers: number[]= [10, 12, 1];
+numbers.push(5)
+
+type Track={
+    name: string,
+    rating: number,
+    isPopular: boolean
+}
+
+const track ={
+    name: 'Track1',
+    rating: 3,
+    isPopular: true
+}
